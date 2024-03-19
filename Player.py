b553ce44a219349d9a01cd841204f4d622662956
Kanisorn-S <karamahati@gmail.com>
2024-03-19 13:12:17 +0700

    I want to play game

--- Player.py
+++ Player.py
@@ -1,3 +1,12 @@
 
 class Player:
-    def __init__()
+    def __init__(self, id, name):
+        self.id = id
+        self.name = name
+        self.items = []
+        self.health = 3
+        self.evasiveness = 0
+        
+    def update():
+        
+        
