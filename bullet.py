0ce49ed85c4212128983c0b3c1bc87972fd0b4d7
Kanisorn-S <karamahati@gmail.com>
2024-03-27 23:51:58 +0700

    Implemented init function

--- bullet.py
+++ bullet.py
@@ -3,7 +3,23 @@ from pygame.locals import *
 import random
 
 class Bullet:
-    def __init__(self, odds):
-        self.types = [0, 1]
+    # Class variable for the type of bullet
+    BLANK = 0
+    LIVE = 1
+    def __init__(self, odds, image):
+        '''
+        Initialize a single bullet
+        Input : odds - A tuple of odds. The first is the probability of the bullet being a blank, 
+                       the second is the probability of the bullet being a live
+                image - A loaded image of the bullet flying
+        Attributes : types - A list of types of bullet
+                     type - The type of this bullet
+                     damage - The damage of this bullet
+        '''
+        self.image = image
+        self.types = [Bullet.BLANK, Bullet.LIVE]
         self.type = random.choices(self.types, odds)
+        self.damage = 1
+    
+
     
