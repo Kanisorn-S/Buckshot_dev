0c7ad09daae6f33ded46856944943efabfabd0f6
Kanisorn-S <karamahati@gmail.com>
2024-03-27 23:24:55 +0700

    player class to be implemented

--- Player.py
+++ Player.py
@@ -6,24 +6,45 @@ PHEIGHT = 100
 
 class Player:
     def __init__(self, window, image, loc, id, name):
-        self.window = window
-        self.image = self.scale(image)
-        self.rect = self.image.get_rect()
-        self.rect.center = loc
-        self.id = id
-        self.name = name
-        self.items = []
-        self.health = 3
-        self.evasiveness = 0
-        
-    def scale(self, image):
-        scaled = pg.image.load(image)
-        scaled = pg.transform.scale(scaled, (PWIDTH, PHEIGHT))
-        return scaled
-        
+        '''
+        Initialize a player.
+        Input : window - The main playing window
+                image - The image of the player, already loaded by pg.image.load
+                loc - The coordinates of the CENTER of the player image
+                id - The id number use to identify a player. In a game with 2 players,
+                      the first player has an index of 0 and the second player has an index of 1
+                name - A string that represents the name of the player. Used to display on top as a name tag 
+                      and at the end screen when the winner is announced
+        '''
+        # TODO
+        pass
+
+    def shot(self, bullet):
+        '''
+        The function updates the players health according to the type of bullet being shot
+        Input : bullet - An object of bullet class with the attributes type and damage
+        If the type is "LIVE", the player's health is subtracted by the bullet's damage
+        If the type is "BLANK", the player's health remains unchanged
+        If the player has evasiveness, use it to determine whether the player takes damage from
+        live rounds or not
+        '''      
+        # TODO
+        pass
+
     def update(self):
+        '''
+        Update the player's health images to be displayed.
+        Might implement more stuff later
+        '''
+        # TODO
         pass
 
     def draw(self):
-        self.window.blit(self.image, self.rect.topleft)
+        '''
+        Draws the player on the window at loc,
+              the player's name tag on top of the players head,
+              the player's health bar at TBD.
+        '''
+        # TODO
+        pass
         
