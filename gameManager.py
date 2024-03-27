989697df259dada9776df1473e89bbd8cf79580a
Kanisorn-S <karamahati@gmail.com>
2024-03-27 23:51:24 +0700

    Added descriptive docstring

--- gameManager.py
+++ gameManager.py
@@ -6,6 +6,22 @@ from item import Item
 
 class GameManager:
     def __init__(self, window, nplayers, players_pic, nbullets, live_round, blank_round, gun):
+        '''
+        Initialize the gameManager.
+        Input : window - The main display window of the game
+                nplayers - The number of players in the game
+                players_pic - A list of tuple, each tuple containing a normal player pic and a green outlined player pic
+                nbullets - The number of bullets in the gun
+                live_round - A loaded picture of a live round
+                blank_round - A loaded picture of a blank round
+                gun - A loaded picture of the cannon
+        Attributes : locations - A list of coordinates of the center of each players
+                     playersInfo - A dictionary where the key is the player and the value is the player's health
+                     players - A list of players
+                     gun - The gun object 
+                     turn - An int that is the index of the player whose turn it currently is
+                     winner - None by default. It is set to a player when that player is the last man standing
+        '''
         self.window = window
         self.nplayers = nplayers
         self.nbullets = nbullets
@@ -21,6 +37,9 @@ class GameManager:
 
         
     def loadPlayer(self):
+        '''
+        Create nplayers Player and return a dictionary with the keys being players and the values being their corresponding health
+        '''
         Players = {}
         for i in range(self.nplayers):
             player = Player(self.window, self.players_pic[i], self.locations[i], i, f"Player{i}")
@@ -39,17 +58,30 @@ class GameManager:
             self.gun.aimLeft()
             
     def update(self):
+        '''
+        Loop through all the players.
+        Calls update on each player then update the health of each player in the playersInfo dictionary.
+        If a player's health reaches zero, remove them from the game.
+        Check if there's only one player standing, set them as the winner.
+        Update the gun (run cannon reload annimation sprite)
+        '''
         for player in self.players:
             player.update()
             self.playersInfo[player] = player.health
             if self.playersInfo[player] <= 0:
                 del self.playersInfo[player]
+            if player == self.players[self.turn]:
+                player.isTurn = True
         if not len(self.playersInfo.keys()):
             self.winner = self.playersInfo.keys()[0]
         self.gun.update()
         
 
     def draw(self):
+        '''
+        Draws all the components by calling draw() on all objects.
+        If a winner is determined, show end screen.
+        '''
         if self.winner is None:
             for player in self.players:
                 player.draw()
