030100ad9169310bcbecbae916faa5feacc6521c
Kanisorn-S <karamahati@gmail.com>
2024-04-18 18:26:28 +0700

    finish ending screen implementation

--- src/ending.py
+++ src/ending.py
@@ -15,6 +15,12 @@ NPLAYER = 2
 
 # Load necessary images
 background = pg.image.load('images/background.jpg')
+gameover = pg.image.load('images/game_over.png')
+gameover = pg.transform.scale_by(gameover, 0.5)
+p1win = pg.image.load('images/p1_wins.png')
+p1win = pg.transform.scale_by(p1win, 0.5)
+p2win = pg.image.load('images/p2_wins.png')
+p2win = pg.transform.scale_by(p2win, 0.5)
 
     
 
@@ -22,10 +28,19 @@ def ending_menu(gameManager: GameManager) -> int:
     clock = pg.time.Clock()
     window = pg.display.get_surface()
     restartButton = pw.CustomButton(window, (300, 300), 'images/restart.png')
-    restartButton.setCenteredLoc((300, 200))
+    restartButton.setCenteredLoc((300, 290))
     backToMenuButton = pw.CustomButton(window, (300, 300), 'images/exit.png')
-    backToMenuButton.setCenteredLoc((300, 300))
+    backToMenuButton.setCenteredLoc((300, 335))
     winner = gameManager.winner
+    image = None
+    if winner.id == 0: # Player 1 wins
+        image = p1win
+    else:
+        image = p2win
+    image_rect = image.get_rect()
+    gameover_rect = gameover.get_rect()
+    image_rect.center = (300, 100)
+    gameover_rect.center = (300, 50)
 
     while True:
         for event in pg.event.get():
@@ -43,11 +58,12 @@ def ending_menu(gameManager: GameManager) -> int:
         winner.updateWin()
         
         window.blit(background, (0, 0))
+        window.blit(gameover, gameover_rect)
+        window.blit(image, image_rect)
         
-
         backToMenuButton.draw()
         restartButton.draw()
-        winner.drawWin(window, (300, 100))
+        winner.drawWin(window, (300, 190))
 
         
         pg.display.update()
