e38dc4a39360c1b68d77bbfcf3552a9177c7cd28
Kanisorn-S <karamahati@gmail.com>
2024-04-21 16:52:45 +0700

    complete basic scroll menu

--- src/new_scrollbar.py
+++ src/new_scrollbar.py
@@ -14,18 +14,27 @@ class ScrollText:
         self.scroll = 0
         self.surface = pg.Surface((self.w + 30, self.h))
         self.surface.set_colorkey('black')
-        # self.text = pw.DisplayText(self.surface, (0, 0), text, textColor='white', fontName='comicsans', fontSize=14)
         self.text = text
         self.tab_rect = pg.Rect(self.surface.get_width() - 20, 0, 20, self.surface.get_height())
         self.slider_rect = pg.Rect(self.surface.get_width() - 20, 0, 20, 40)
+        self.text_surface = font.render(self.text, False, 'white')
+        self.speed = 5
+        self.rect = self.surface.get_rect()
 
     def handleEvent(self, event: pg.event):
-        pass
+        if event.type == pg.MOUSEWHEEL:
+            # event.y is 1 when scrolling up, -1 when scrolling down
+            if not self.rect.collidepoint(pg.mouse.get_pos()):
+                print(event.y)
+                self.scroll = min(self.scroll + event.y * self.speed, 0)
+
     
     def update(self):
-        self.text_surface = font.render(self.text, False, 'white')
+
+        self.slider_rect.top = max(-self.scroll, 0)
 
     def draw(self):
+        self.surface.fill('black')
         pg.draw.rect(self.surface, "gray", self.tab_rect)
         pg.draw.rect(self.surface, "green", self.slider_rect)
         self.surface.blit(self.text_surface, (0, self.scroll))
